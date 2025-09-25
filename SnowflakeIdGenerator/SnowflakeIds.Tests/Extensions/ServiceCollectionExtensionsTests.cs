4776fd121f9feaf711d7d0c60991d74ddb6dca69
josedanielcr <jdcanales1905@gmail.com>
2025-09-24 22:00:06 -0600

    Feat: Allowed decoupling of configuration naming from client, by adding extension method with custom configuration for the package

---
+++ SnowflakeIdGenerator/SnowflakeIds.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using SnowflakeIds.Abstractions;
+using SnowflakeIds.Common;
+using SnowflakeIds.Extensions;
+
+namespace SnowflakeIds.Tests.Extensions;
+
+public class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void GivenValidConfiguration_WhenAddingSnowflakeIdsDependency_ThenShouldRegistersGeneratorDependencies()
+    {
+        //Assert
+        var services = new ServiceCollection();
+        services.AddSnowflakeIds(options =>
+        {
+            options.StartYear = 2025;
+            options.StartMonth = 1;
+            options.StartDay = 1;
+            options.TimestampLength = 41;
+            options.SequenceLength = 12;
+            options.WorkerLength = 10;
+            options.WorkerId = 1;
+        });
+        
+        //Act
+        using var provider = services.BuildServiceProvider();
+        var generator = provider.GetRequiredService<IIdGenerator>();
+        var settings = provider.GetRequiredService<IOptions<Settings>>().Value;
+        
+        //Assert
+        generator.Next().Should().BeGreaterThan(0);
+        settings.WorkerId.Should().Be(1);
+        settings.TimestampLength.Should().Be(41);
+    }
+
+    [Fact]
+    public void GivenNoConfiguration_WhenAddingSnowflakeIdsDependency_ThenShouldAllowsManualOptionsConfiguration()
+    {
+        //Arrange
+        var services = new ServiceCollection();
+        services.AddSnowflakeIds();
+        services.PostConfigure<Settings>(options =>
+        {
+            options.StartYear = 2025;
+            options.StartMonth = 1;
+            options.StartDay = 1;
+            options.TimestampLength = 41;
+            options.SequenceLength = 12;
+            options.WorkerLength = 10;
+            options.WorkerId = 1;
+        });
+        
+        // Act
+        using var provider = services.BuildServiceProvider();
+        var generator = provider.GetRequiredService<IIdGenerator>();
+        
+        // Assert
+        generator.Next().Should().BeGreaterThan(0);
+    }
+}
---
+++ SnowflakeIdGenerator/SnowflakeIds/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using SnowflakeIds.Abstractions;
+using SnowflakeIds.Common;
+using SnowflakeIds.Implementations;
+
+namespace SnowflakeIds.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static OptionsBuilder<Settings> AddSnowflakeIds(this IServiceCollection services, Action<Settings> configure)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configure);
+
+        return services.AddSnowflakeIds().Configure(configure);
+    }
+    
+    public static OptionsBuilder<Settings> AddSnowflakeIds(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.TryAddSingleton<ITimestampGenerator, TimestampGenerator>();
+        services.TryAddSingleton<ISequenceManager, SequenceManager>();
+        services.TryAddSingleton<ISnowflakeComposer, SnowflakeComposer>();
+        services.TryAddSingleton<IIdGenerator, IdGenerator>();
+
+        return services.AddOptions<Settings>();
+    }
+}
